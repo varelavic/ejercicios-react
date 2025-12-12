8ce27ebd8bea33493477ff5c69c2bb3f572d71c5
Victor Varela <varelavv@outlook.es>
2025-12-12 20:39:33 +0100

    base del proyecto

---
+++ ejerciciosReact/src/App.jsx
@@ -0,0 +1,12 @@
+import Title from './components/Title.jsx';
+
+function App() {
+  return (
+    <div className="w-full min-h-screen bg-gray-900 p-4">
+      <Title title="Mi Proyecto en React" />
+    </div>
+  )
+}
+
+
+export default App
---
+++ ejerciciosReact/src/components/Title.jsx
@@ -0,0 +1,6 @@
+const Title = ({title}) => {
+    return (
+        <h1 className="text-2xl font-bold text-white text-center pt-10">{title}</h1>
+    );
+};
+export default Title;
