0b9d672f5bb36b16985c18f6cb8a2ece097f7e59
Victor Varela <varelavv@outlook.es>
2025-12-13 20:16:39 +0100

    Solucionando fallos del deploy

--- ejerciciosReact/vite.config.js
+++ ejerciciosReact/vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   base: '/ejerciciosReact/',
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
 })
